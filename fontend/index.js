b78ef671ebddc2c17ea27a276847e1b019b63fe2
Simon Kallkvist <simon.kallkvist@gmail.com>
2024-05-14 12:05:15 +0200

    forthCommit

--- fontend/index.js
+++ fontend/index.js
@@ -2,12 +2,24 @@
 
 let userInfo = document.querySelector('.userInfo');
 
-// Login form change btn
-
+// Login form change btns
 document.querySelector('.registerUserBtn').addEventListener('click', () => {
   document.querySelector('.loginUser').classList.toggle('hidden');
   document.querySelector('.registerUser').classList.toggle('hidden');
+  resetForm();
 });
+document.querySelector('.backBtn').addEventListener('click', () => {
+  document.querySelector('.loginUser').classList.toggle('hidden');
+  document.querySelector('.registerUser').classList.toggle('hidden');
+  resetForm();
+});
+
+let resetForm = () => {
+  let inputs = document.querySelectorAll('input');
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+};
 
 // When Create user is clicked
 document.querySelector('.createUser').addEventListener('click', () => {
@@ -75,6 +87,11 @@ document.querySelector('.loginUserBtn').addEventListener('click', () => {
   }
 });
 
+// When logut btn is pressed
+// document.querySelector('.logOut').addEventListener('click', () => {
+//   logoutUser();
+// });
+
 // Create the user and add them to Strapi
 let createUser = async (username, email, password) => {
   console.log(username, email, password);
@@ -114,32 +131,101 @@ let loginUser = async (username, password) => {
   }
 };
 
+let logoutUser = () => {
+  sessionStorage.clear();
+  checkStorage();
+};
+
+// When looged in show btns in nav and set username
 let setLoginScreen = () => {
   // remove the loginForm if there is a token in sessionstorage
   if (sessionStorage.getItem('token')) {
     document.querySelector('.loginForm').classList.add('hidden');
+
+    let userInfo = document.querySelector('.userInfo');
+    userInfo.innerHTML = '';
+    userInfo.classList.remove('hidden');
+
+    let username = sessionStorage.getItem('username');
+
+    let profileTag = document.createElement('h4');
+    profileTag.classList.add('profileTag');
+    profileTag.innerText = username;
+
+    let profileLink = document.createElement('a');
+    profileLink.href = 'profile.html';
+    profileLink.innerText = 'Profile';
+    let icon = document.createElement('i');
+    icon.style.fontSize = '32px';
+    icon.style.marginRight = '-24px';
+    icon.classList.add('fa-solid', 'fa-user');
+
+    let logOutBtn = document.createElement('button');
+    logOutBtn.classList.add('btn', 'logOutBtn');
+    logOutBtn.innerText = 'Logout';
+    logOutBtn.addEventListener('click', () => {
+      logoutUser();
+    });
+
+    userInfo.append(icon, profileTag, profileLink, logOutBtn);
   }
-
-  let userInfo = document.querySelector('.userInfo');
-  userInfo.classList.remove('hidden');
-
-  let username = sessionStorage.getItem('username');
-
-  let profileTag = document.createElement('h4');
-  profileTag.innerText = username;
-
-  userInfo.append(profileTag);
 };
 
+let resetLogin = () => {
+  document.querySelector('.loginForm').classList.remove('hidden');
+  let userInfo = document.querySelector('.userInfo');
+  //   userInfo.remove('profileTag');
+  userInfo.classList.add('hidden');
+};
+
+// check if someone is logged in
 let checkStorage = () => {
   if (sessionStorage.getItem('token') && sessionStorage.getItem('token')) {
     setLoginScreen();
+  } else {
+    resetLogin();
   }
 };
 
+// get all books and loop them Print cards
 let setStartScreen = async () => {
-  let books = await getData('http://localhost:1337/api/books?populate=*');
+  let book = await getData('http://localhost:1337/api/books?populate=*');
+  let books = Array.from(book.data.data);
   console.log(books);
+
+  let bookContainer = document.querySelector('.bookContainer');
+
+  if (books) {
+    books.map((book) => {
+      let card = document.createElement('div');
+      card.classList.add('card');
+
+      let bookCover = document.createElement('img');
+      bookCover.src =
+        'http://localhost:1337' + book.attributes.cover.data.attributes.url;
+      let cardDiv = document.createElement('div');
+
+      let title = document.createElement('h3');
+      title.innerText = book.attributes.title;
+
+      let author = document.createElement('h4');
+      author.innerText = book.attributes.author;
+
+      let pages = document.createElement('p');
+      pages.innerText = book.attributes.pages + ' pages';
+
+      let year = document.createElement('p');
+      year.innerText = 'Release: ' + book.attributes.date;
+
+      let seeMoreBtn = document.createElement('button');
+      seeMoreBtn.classList.add('btn', 'seeMore');
+      seeMoreBtn.innerText = 'See more';
+
+      cardDiv.append(title, author, pages, year, seeMoreBtn);
+      card.append(bookCover, cardDiv);
+      bookContainer.append(card);
+    });
+  }
 };
 
 setStartScreen();
